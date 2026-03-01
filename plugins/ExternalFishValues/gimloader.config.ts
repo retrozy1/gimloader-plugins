39a767810644c59c15349a794c81cce0c0770f21
retrozy1 <202344625+retrozy1@users.noreply.github.com>
2026-02-28 19:27:50 -0500

    Add ExternalFishValues

---
+++ plugins/ExternalFishValues/gimloader.config.ts
@@ -0,0 +1,8 @@
+import { pluginConfig } from '../../shared/config.ts';
+
+export default pluginConfig({
+  name: 'ExternalFishValues',
+  description: 'plugin for benji',
+  version: '1.0.0',
+  gamemodes: ['fishtopia'],
+});
---
+++ plugins/ExternalFishValues/src/index.ts
@@ -0,0 +1,52 @@
+let started = false;
+
+const fishValues = {
+    "gray": 1,
+    "green": 2,
+    "red": 5,
+    "blue": 10,
+    "purple": 20,
+    "beach": 40,
+    "star": 65,
+    "galaxy": 100,
+    "berry": 150,
+    "gim": 5000
+};
+
+function start(char: any) {
+  const div = document.createElement("div")
+  div.className = "bg-white absolute right-4 top-1/2 -translate-y-1/2";
+
+  const interval = setInterval(() => {
+    let total = 0;
+
+    for(const [id, { amount }] of char.inventory.slots) {
+        if(!id.endsWith("-fish")) continue;
+        const fishName = id.split("-")[0];
+        total += fishValues[fishName as keyof typeof fishValues] * amount;
+    }
+
+    div.innerHTML = `without multiplier: $${total}_____________________with multiplier: $${Math.round(total * 1.3)}`;
+  }, 500)
+
+  api.onStop(() => {
+    clearInterval(interval)
+  })
+
+  document.body.appendChild(div);
+  api.onStop(() => {
+    document.body.removeChild(div);
+  })
+}
+
+if (api.net.isHost) {
+  api.net.onLoad(() => {
+    api.onStop(
+      api.net.room.state.characters.onAdd((char: any) => {
+        if (started || char.id === api.stores.network.authId) return;
+        started = true;
+        start(char);
+      }, true)
+    )
+  })
+}
